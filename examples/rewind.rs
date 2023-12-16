74baa8fb66e5617a30407449fc7c8178ccf719c4
Antoine Plard <antoineplddev@gmail.com>
2023-12-16 11:31:29 +0100

    init

---
+++ examples/rewind.rs
@@ -0,0 +1,15 @@
+use rewind::rewind;
+use tracing::Level;
+
+fn init_log() {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
+        .init();
+}
+
+#[tokio::main]
+async fn main() {
+    init_log();
+
+    let _rewind = rewind("pcap/capture.pcapng");
+}
